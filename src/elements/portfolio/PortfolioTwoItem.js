976e61243436b30774776e4f2d3fa71c9c41790f
minionboss0626 <minionboss0626@gmail.com>
2023-04-11 07:40:13 +0800

    added build folder

--- src/elements/portfolio/PortfolioTwoItem.js
+++ src/elements/portfolio/PortfolioTwoItem.js
@@ -2,33 +2,41 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const PortfolioTwoItem = ({ portfolio }) => {
-  console.log(portfolio)
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log(portfolio.url)
+    window.open(portfolio.url, '_blank');
+  };
+
   return (
-    <div className="rwt-card p-5 h-100">
+    <div className="rwt-card p-3 h-100">
       <div className="inner d-flex flex-column justify-content-between h-100">
         <div className="thumbnail d-flex flex-column justify-content-center rounded-3 p-3 bg-light bg-gradient h-100">
           <figure className="card-image">
-            <Link
-              to={portfolio.url}
+            <a
+              href={'https://' + portfolio.url}
+              onClick={handleClick}
             >
               <img
                 className="img-fluid"
                 src={portfolio.image}
                 alt="Portfolio-01"
               />
-            </Link>
+            </a>
           </figure>
-          <Link
-            to={portfolio.url}
-          ></Link>
+          <a
+            href={'https://' + portfolio.url}
+            onClick={handleClick}
+          ></a>
         </div>
         <div className="content text-center">
           <h5 className="title mb--10">
-            <Link
-              to={portfolio.url}
+            <a
+              href={'https://' + portfolio.url}
+              onClick={handleClick}
             >
               {portfolio.url}
-            </Link>
+            </a>
           </h5>
         </div>
       </div>
